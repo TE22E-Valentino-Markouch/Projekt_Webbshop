ed82674f09e1da4a620bbda870aa0301280ff2c5
Valentino Markouch <Valentino.Markouch@elev.ga.ntig.se>
2024-03-06 14:55:25 +0100

    formaterat js

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-
-
 function add(namn, pris) {
-    // Hämta element
-    const varukorgLista = document.getElementById("Varukorg-listan");
-    const totalprisElement = document.getElementById("Totalpris");
-  
-    // Skapa element för produkten
-    const produktElement = document.createElement("ul");
-    produktElement.textContent = namn + ": " + pris + " kr";
-  
-    // Lägg till produkt i listan
-    varukorgLista.appendChild(produktElement);
-  
-    // Uppdatera totalpriset
-    const nyttTotalpris = parseInt(totalprisElement.textContent) + pris + "kr";
-    totalprisElement.textContent = nyttTotalpris;
-  }
+  // Hämta element
+  const varukorgLista = document.getElementById("Varukorg-listan");
+  const totalprisElement = document.getElementById("Totalpris");
+
+  // Skapa element för produkten
+  const produktElement = document.createElement("ul");
+  produktElement.textContent = namn + ": " + pris + " kr";
+
+  // Lägg till produkt i listan
+  varukorgLista.appendChild(produktElement);
+
+  // Uppdatera totalpriset
+  const nyttTotalpris = parseInt(totalprisElement.textContent) + pris + "kr";
+  totalprisElement.textContent = nyttTotalpris;
+}
